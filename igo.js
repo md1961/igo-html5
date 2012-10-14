799a3f609024ee9fbe1b52556bdecf3b3ce30465
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2012-10-14 11:48:03 +0900

    [igo.js] Introduce margin for board

--- igo.js
+++ igo.js
@@ -4,26 +4,31 @@ onload = function() {
 };
 
 var dimension = {
-  originX: 20,
-  originY: 20,
-  gridPitch: 20,
+  margin: 10,
+  gridPitch: 40,
   numGrids: 19,
+
+  originX: function() { return this.margin + this.gridPitch / 2; },
+  originY: function() { return this.margin + this.gridPitch / 2; },
 }
 
 function init(canvas) {
   var dim = dimension;
 
   canvas.beginPath();
-  var x1 = dim.originX + dim.gridPitch * (dim.numGrids - 1);
-  var y1 = dim.originY + dim.gridPitch * (dim.numGrids - 1);
+
+  var x1 = dim.originX() + dim.gridPitch * (dim.numGrids - 1);
+  var y1 = dim.originY() + dim.gridPitch * (dim.numGrids - 1);
   for (i = 0; i < dim.numGrids; i++) {
-    var x0 = dim.originX + dim.gridPitch * i;
-    canvas.moveTo(x0, dim.originY);
+    var x0 = dim.originX() + dim.gridPitch * i;
+    canvas.moveTo(x0, dim.originY());
     canvas.lineTo(x0, y1);
-    var y0 = dim.originY + dim.gridPitch * i;
-    canvas.moveTo(dim.originX, y0);
-    canvas.lineTo(x1         , y0);
+
+    var y0 = dim.originY() + dim.gridPitch * i;
+    canvas.moveTo(dim.originX(), y0);
+    canvas.lineTo(x1           , y0);
   }
+
   canvas.closePath();
 
   canvas.stroke();
