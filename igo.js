96f79fbe64e511f0b398051f482d9e8cef6dd6a8
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2012-10-14 19:42:00 +0900

    (tmp) Draw grid dynamically preliminarily

--- igo.js
+++ igo.js
@@ -1,27 +1,5 @@
 onload = function() {
-  //var canvas = get_canvas_context('board');
-  //init(canvas);
-
-  for (i = 0; i < 4; i++) {
-    canvas0 = get_canvas_context('canvas0' + i);
-    canvas0.beginPath();
-    canvas0.moveTo( 0, 11);
-    canvas0.lineTo(21, 11);
-    canvas0.moveTo(11,  0);
-    canvas0.lineTo(11, 21);
-    canvas0.closePath();
-    canvas0.stroke();
-  }
-  /*
-    canvas1 = get_canvas_context('canvas01');
-    canvas1.beginPath();
-    canvas1.moveTo( 0, 11);
-    canvas1.lineTo(21, 11);
-    canvas1.moveTo(11,  0);
-    canvas1.lineTo(11, 21);
-    canvas1.closePath();
-    canvas1.stroke();
-  */
+  init("main_board");
 };
 
 var dimension = {
@@ -33,26 +11,31 @@ var dimension = {
   originY: function() { return this.margin + this.gridPitch / 2; },
 }
 
-function init(canvas) {
-  var dim = dimension;
+function init(table_id) {
+  var table = document.getElementById(table_id);
+  for (i = 0; i < 9; i++) {
+    var row = document.createElement('tr');
+    table.appendChild(row);
+    for (j = 0; j < 9; j++) {
+      var id = 'g' + i + j;
+      var cell = document.createElement('td');
+      row.appendChild(cell);
+      var canvas = document.createElement('canvas');
+      cell.appendChild(canvas);
+      canvas.id = id;
+      canvas.width  = 21;
+      canvas.height = 21;
 
-  canvas.beginPath();
-
-  var x1 = dim.originX() + dim.gridPitch * (dim.numGrids - 1);
-  var y1 = dim.originY() + dim.gridPitch * (dim.numGrids - 1);
-  for (i = 0; i < dim.numGrids; i++) {
-    var x0 = dim.originX() + dim.gridPitch * i;
-    canvas.moveTo(x0, dim.originY());
-    canvas.lineTo(x0, y1);
-
-    var y0 = dim.originY() + dim.gridPitch * i;
-    canvas.moveTo(dim.originX(), y0);
-    canvas.lineTo(x1           , y0);
+      cxt = get_canvas_context(id);
+      cxt.beginPath();
+      cxt.moveTo( 0, 11);
+      cxt.lineTo(21, 11);
+      cxt.moveTo(11,  0);
+      cxt.lineTo(11, 21);
+      cxt.closePath();
+      cxt.stroke();
+    }
   }
-
-  canvas.closePath();
-
-  canvas.stroke();
 }
 
 
