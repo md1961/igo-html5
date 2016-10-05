87bf64ee3ee8bf50dde94456096cddd9bf33dd96
Naoyuki Kumagai <kumagai@namaka>
2016-10-05 16:35:28 +0900

    Rename instance variable moves of MoveSet to _moves

--- app/move_set.js
+++ app/move_set.js
@@ -6,9 +6,9 @@ function MoveSet() {
   this.VALID_MODES = [this.MODE_TURN, this.MODE_PLAY, this.MODE_TEMP];
 
   this.clear = function() {
-    this.title = "";
-    this.inits = [];
-    this.moves = new Moves([]);
+    this.title  = "";
+    this.inits  = [];
+    this._moves = new Moves([]);
   };
 
   this.clear();
@@ -31,7 +31,7 @@ function MoveSet() {
   };
 
   this.length = function() {
-    return this.moves.length();
+    return this._moves.length();
   };
 
   this.numCurrentMove = function() {
@@ -59,14 +59,14 @@ function MoveSet() {
     if (stonesTaken.length > 0) {
       move += '(' + stonesTaken.join(',') + ')';
     }
-    this.moves.push(move);
+    this._moves.push(move);
   };
 
   this.popLastMove = function() {
-    if (this.moves.length() === 0) {
+    if (this._moves.length() === 0) {
       return null;
     }
-    return this.moves.pop();
+    return this._moves.pop();
   };
 
   this._startMode = function(mode) {
@@ -98,17 +98,17 @@ function MoveSet() {
   };
 
   this._startTempMode = function() {
-    this._moves_saved = this.moves;
-    this.moves = new Moves([]);
+    this._moves_saved = this._moves;
+    this._moves = new Moves([]);
     //TODO: _indexPlaySaved is unused while isTempMode is true ????
     this._indexPlaySaved = this._indexPlay;
   };
 
   this._finishTempMode = function() {
-    if (this.moves.length() > 0 && confirm("この検討手順を分岐として保存しますか?")) {
-      this._moves_saved.insert(this._indexPlaySaved, this.moves.strMoves());
+    if (this._moves.length() > 0 && confirm("この検討手順を分岐として保存しますか?")) {
+      this._moves_saved.insert(this._indexPlaySaved, this._moves.strMoves());
     }
-    this.moves = this._moves_saved;
+    this._moves = this._moves_saved;
   };
 
   this.numMovesToPlay = function() {
@@ -116,11 +116,11 @@ function MoveSet() {
   };
 
   this.playNext = function() {
-    if (this._indexPlay >= this.moves.length()) {
-      this._indexPlay = this.moves.length();
+    if (this._indexPlay >= this._moves.length()) {
+      this._indexPlay = this._moves.length();
       return null;
     }
-    return this.moves.get(this._indexPlay++);
+    return this._moves.get(this._indexPlay++);
   };
 
   this.playPrev = function() {
@@ -129,21 +129,21 @@ function MoveSet() {
       return null;
     }
     this._indexPlay--;
-    return this.moves.get(this._indexPlay);
+    return this._moves.get(this._indexPlay);
   };
 
   this.DEFAULT_NEXT_TURN = BLACK;
 
   this.nextTurn = function() {
-    if (this.moves.length() === 0) {
+    if (this._moves.length() === 0) {
       return this.DEFAULT_NEXT_TURN;
     } else if (this._mode == this.MODE_PLAY) {
-      if (this._indexPlay < this.moves.length()) {
-        return getColorOfStone(this.moves.get(this._indexPlay));
+      if (this._indexPlay < this._moves.length()) {
+        return getColorOfStone(this._moves.get(this._indexPlay));
       }
-      return getOpponent(getColorOfStone(this.moves.get(this._indexPlay - 1)));
+      return getOpponent(getColorOfStone(this._moves.get(this._indexPlay - 1)));
     } else {
-      var lastStrMove = this.moves.get(this.moves.length() - 1);
+      var lastStrMove = this._moves.get(this._moves.length() - 1);
       //TODO: Use getOpponent()?
       switch (lastStrMove[0].toUpperCase()) {
         case  NONE[0]: return null;
@@ -155,7 +155,7 @@ function MoveSet() {
   };
 
   this.branches = function() {
-    return this.moves.branches(this._indexPlay);
+    return this._moves.branches(this._indexPlay);
   };
 
   this.offsetToNextJunction = function() {
@@ -168,8 +168,8 @@ function MoveSet() {
 
   this._offsetToAdjacentJunction = function(direction) {
     direction = direction / Math.abs(direction);
-    for (var i = this._indexPlay + direction; 0 <= i && i <= this.moves.length(); i += direction) {
-      if (this.moves.branches(i).length > 0) {
+    for (var i = this._indexPlay + direction; 0 <= i && i <= this._moves.length(); i += direction) {
+      if (this._moves.branches(i).length > 0) {
         return i - this._indexPlay;
       }
     }
@@ -177,8 +177,8 @@ function MoveSet() {
   };
 
   this.branchTo = function(numBranch) {
-    this._strMovesToRewind = this.moves.branches(this._indexPlay)[numBranch];
-    this.moves.branchTo(this._indexPlay, numBranch);
+    this._strMovesToRewind = this._moves.branches(this._indexPlay)[numBranch];
+    this._moves.branchTo(this._indexPlay, numBranch);
     //TODO: Handle branch on branch...
     this._indexPlaySaved = this._indexPlay;
     this._indexPlay = 0;
@@ -190,7 +190,7 @@ function MoveSet() {
     if (! this.onBranch() || this._indexPlaySaved === null) {
       return;
     }
-    this.moves.backToTrunk();
+    this._moves.backToTrunk();
     this._strMovesToRewind = this._strMovesToRewind.slice(0, this._indexPlay);
     this._indexPlay = this._indexPlaySaved;
     this._indexPlaySaved = null;
@@ -202,7 +202,7 @@ function MoveSet() {
       return;
     }
     this.backToTrunk();
-    this.moves.removeBranch(this._indexPlay, this._numBranch);
+    this._moves.removeBranch(this._indexPlay, this._numBranch);
   };
 
   this.strMovesToRewind = function() {
@@ -210,24 +210,24 @@ function MoveSet() {
   };
 
   this.addComment = function(comment) {
-    var _index = (this._mode == this.MODE_TURN) ? this.moves.length() - 1 : this._indexPlay - 1;
-    this.moves.addComment(comment, _index);
+    var _index = (this._mode == this.MODE_TURN) ? this._moves.length() - 1 : this._indexPlay - 1;
+    this._moves.addComment(comment, _index);
   };
 
   this.getCurrentComment = function() {
     if (this._indexPlay <= 0) {
       return null;
-    } else if (this._indexPlay > this.moves.length()) {
-      this._indexPlay = this.moves.length();
+    } else if (this._indexPlay > this._moves.length()) {
+      this._indexPlay = this._moves.length();
     }
-    var move = this.moves.get(this._indexPlay - 1);
+    var move = this._moves.get(this._indexPlay - 1);
     return parseMove(move)[4];
   };
 
   this.readDataInHash = function(hash) {
     this.title = hash.title;
     this.inits = hash.inits;
-    this.moves = new Moves(hash.moves);
+    this._moves = new Moves(hash.moves);
   };
 
   this.readDataInJson = function(json) {
@@ -239,7 +239,7 @@ function MoveSet() {
     return {
       "title": this.title,
       "inits": this.inits,
-      "moves": this.moves.strMoves()
+      "moves": this._moves.strMoves()
     };
   };
 
