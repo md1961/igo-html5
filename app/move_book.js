b4315b37b9b1df1c68e50df3ba8ea47a801f638e
Naoyuki Kumagai <kumagai@namaka>
2016-11-15 13:50:03 +0900

    Add getter and setter name() to MoveBook

--- app/move_book.js
+++ app/move_book.js
@@ -90,4 +90,12 @@ MoveBook.prototype = {
   toJson : function() {
     return JSON.stringify(this.toHash());
   },
+
+  get name () {
+    return this._name;
+  },
+
+  set name (value) {
+    this._name = value;
+  },
 };
